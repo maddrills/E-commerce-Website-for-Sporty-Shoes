35b7d5d310fe4e7be8705c0fd8058184859f9d20
mathew_francis <mathewfvv@gmail.com>
2023-04-02 17:12:04 +0530

    update

---
+++ src/main/java/com/api/sportyShoes/exceptionHandler/BusinessException.java
@@ -0,0 +1,18 @@
+package com.api.sportyShoes.exceptionHandler;
+
+public class BusinessException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1008128726286682480L;
+
+	public BusinessException() {
+		super();
+	}
+
+	public BusinessException(String message) {
+		super(message);
+	}
+
+}
